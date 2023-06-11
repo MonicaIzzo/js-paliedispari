b3f3565d998bc092e74b6df8dec4f6267efc23b2
MonicaIzzo <info@monicaizzo.net>
2023-06-11 19:55:25 +0200

    Add versione PALINDROMA DOM

--- js/script_palidromaDOM.js
+++ js/script_palidromaDOM.js
@@ -8,46 +8,45 @@
 
 console.log ('JS OK');
 
-// #0 dichiaro delle variabili di comodo.
-let parola = document.getElementById ('parola');
-const confirmButton = document.getElementById ('confirm-button');
 let carattere;
 let parolaInversa = '';
-let i;
+
+// #0 recupero gli elementi dal DOM.
+
+const parolaField = document.getElementById('parola');
+const button = document.getElementById('BtnPalindroma');
+const resultElement = document.getElementById('result');
 
 
 
-confirmButton.addEventListener('click', function() {
-const parolaValue = parola.value.trim();
+button.addEventListener ('click', function() {
+  let parola = parolaField.value.trim();
+  console.log(parola);
+  let i = parola.length - 1;
 
-// # 3 Validazione
-if (!parolaValue || !isNaN(parolaValue)) {
-    alert('I dati inseriti non sono validi');
-    return;
-}   
-// #2 estraggo ciascun carattere e lo inserisco in una variabile.
-let i = parola.length - 1;
-})
-
-console.log(parola);
-
-
-
-// #3 con il ciclo while verifico che la parola presa in input è palindroma.
-while (i >= 0) {
+  while (i >= 0) {
   carattere = parola[i];
   parolaInversa += carattere;
   console.log(carattere);
   i--;
 }
-
-// #4 Stampo in consol.log il messaggio.
-console.log(parolaInversa);
+console.log('la parola inversa è ' + parolaInversa);
 
 if(parola == parolaInversa){ // verifico che la condizione sià vera.
 // #SE la parola è polindroma, il messaggio sarà: la parola è palindroma.    
-     alert('BRAVO! la parola inserita è palindroma');
+     resultElement.innerText ='BRAVO! la parola inserita è palindroma';
+     parolaInversa = '';
 // #SE INVECE la parola non è polindroma, il messaggio sarà: la parola non è palindroma.    
   } else {
-     alert('PECCATO! la parola non è palindroma');
+    resultElement.innerText ='PECCATO! la parola non è palindroma';
+    parolaInversa = '';
   }
+console.log(parola);
+console.log(parolaInversa);
+});
+
+
+
+
+
+
